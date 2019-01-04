7f878337d9817bfbb80b5a9d558dbecdcdd27702
Edwin Clement <edwinclement08@gmail.com>
2019-01-05 02:16:37 +0530

    First Initial

---
+++ src/com/edwinclement08/server/ChatServer.java
@@ -0,0 +1,10 @@
+package com.edwinclement08.server;
+
+public class ChatServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing the Server for the first time");
+	}
+
+}
