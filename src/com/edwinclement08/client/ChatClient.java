eb7f91c37a3e792b667c951cbabdc949a78e8a2d
Edwin Clement <edwinclement08@gmail.com>
2019-01-05 02:29:11 +0530

    Started with client side

---
+++ src/com/edwinclement08/client/ChatClient.java
@@ -0,0 +1,10 @@
+package com.edwinclement08.client;
+
+public class ChatClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Client First time run");
+	}
+
+}
